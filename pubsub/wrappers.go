e72d98cfef725f5f3341ce6277ef5f847b5b1266
Luís Nascimento <luisnascimento.dev@gmail.com>
2022-09-26 22:12:20 -0300

    fix(pubsub): Exposing public reference to Topic on TopicWrapper

--- pubsub/wrappers.go
+++ pubsub/wrappers.go
@@ -8,11 +8,11 @@ import (
 
 // TopicWrapper envelopes a pubsub topic type.
 type TopicWrapper struct {
-	topic *pubsub.Topic
+	Topic *pubsub.Topic
 }
 
 // Publish envelopes a pubsub topic publish method.
 func (tw TopicWrapper) Publish(ctx context.Context, msg *pubsub.Message) error {
-	_, err := tw.topic.Publish(ctx, msg).Get(ctx)
+	_, err := tw.Topic.Publish(ctx, msg).Get(ctx)
 	return err
 }
